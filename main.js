0275255c58f07c88cbb9f7083587c733344d2bc9
Hyung97 <tammyhyunglee@gmail.com>
2021-02-17 23:10:39 -0600

    ñam ñam comienzo parte 2

--- main.js
+++ main.js
@@ -1,76 +1,55 @@
-var miDBAlumnos= openDatabase('dbAlumnos','1.0','Aplicacion de Alumnos',5*1024*1024);
-window.id = 0;
-if(!miDBAlumnos){
-    alert("Elnavegador no soporta Web SQL");
-}
-
+var generarIdUnicoDesdeFecha=()=>{
+    let fecha = new Date();//03/02/2021
+    return Math.floor(fecha.getTime()/1000).toString(16);
+}, db;
 var appVue = new Vue({
-
-    el: '#appAlumnos',
-    data: {
-        alumno:{
-            idAlumno  : 0,
-            codigo      : '',
-            nombre       :'',
-            direccion         : '', 
-            municipio         : '', 
-            departamento      : '',    
-            telefono          : '',
-            fechanacimiento   : '',      
-            sexo              : '' 
-        },
-        alumnos:[]
+    el:'#appSistema',
+    data:{
+        forms:{
+            'categoria':{mostrar:false},
+            'producto':{mostrar:false},
+            'cliente':{mostrar:false},
+        }
     },
     methods:{
-        guardarAlumno(){
-            /**
-             * BD Web SQL
-             */
-            miDBAlumnos.transaction(tran=>{
-                tran.executeSql('INSERT INTO alumnos(idAlumno,codigo,nombre,direccion,municipio,departamento,telefono,fechanacimiento,sexo) VALUES(?,?,?,?,?,?,?,?,?) ',
-                    [++id,this.alumno.codigo,this.alumno.nombre,this.alumno.direccion, this.alumno.municipio,this.alumno.departamento,this.alumno.telefono, this.alumno.fechanacimiento,this.alumno.sexo]);
-                this.obtenerAlumnos();
-                this.limpiar();
-            }, err=>{
-                console.log( err );
-            });
-        },
-  
-      
+        abrirBd(){
+            let indexDb = indexedDB.open('db_sistema_facturacion',1);
+            indexDb.onupgradeneeded=event=>{
+                let req=event.target.result,
+                    tblproductos = req.createObjectStore('tblproductos',{keyPath:'idProducto'}),
+                    tblcategorias = req.createObjectStore('tblcategorias',{keyPath:'idCategoria'}),
+                    tblclientes = req.createObjectStore('tblclientes',{keyPath:'idCliente'}),
+                    tblproveedores = req.createObjectStore('tblproveedores',{keyPath:'idProveedor'});
+                tblproductos.createIndex('idProducto','idProducto',{unique:true});
+                tblproductos.createIndex('codigo','codigo',{unique:false});
+                
+                tblcategorias.createIndex('idCategoria','idCategoria',{unique:true});
+                tblcategorias.createIndex('codigo','codigo',{unique:false});
 
-        obtenerAlumnos(){
-            miDBAlumnos.transaction(tran=>{
-                tran.executeSql('SELECT * FROM alumnos',[],(index,data)=>{
-                    this.alumnos = data.rows;
-                    id=data.rows.length;
-                });
-            }, err=>{
-                console.log( err );
-            });
-        },
-        
-        mostrarAlumno(almn){
-            this.alumno = almn;
-        },
+                tblclientes.createIndex('idCliente','idCliente',{unique:true});
+                tblclientes.createIndex('codigo','codigo',{unique:false});
 
-        limpiar(){
-            this.alumno.codigo='';
-            this.alumno.nombre='';
-            this.alumno.direccion='';
-            this.alumno.municipio='';
-            this.alumno.departamento='';
-            this.alumno.telefono='';
-            this.alumno.fechanacimiento='';
-            this.alumno.sexo='';
-
+                tblproveedores.createIndex('idProveedor','idProveedor',{unique:true});
+                tblproveedores.createIndex('codigo','codigo',{unique:false});
+            };
+            indexDb.onsuccess = evt=>{
+                db=evt.target.result;
+            };
+            indexDb.onerror=e=>{
+                console.log("Error al conectar a la BD", e);
+            };
         }
     },
     created(){
-        miDBAlumnos.transaction(tran=>{
-            tran.executeSql('CREATE TABLE IF NOT EXISTS alumnos(idAlumno int PRIMARY KEY NOT NULL, codigo varchar(10), nombre varchar(60),direccion varchar(60),municipio varchar(50),departamento varchar(50),telefono varchar(10),fechanacimiento date(), sexo varchar(20))');
-        }, err=>{
-            console.log( err );
-        });
-        this.obtenerAlumnos();
+        this.abrirBd();
     }
+});
+
+document.addEventListener("DOMContentLoaded",event=>{
+    let el = document.querySelectorAll(".mostrar").forEach( (element, index)=>{
+        element.addEventListener("click",evt=>{
+            appVue.forms[evt.target.dataset.form].mostrar = true;
+            appVue.$refs[evt.target.dataset.form].obtenerDatos();
+        });
+    } );
 });
